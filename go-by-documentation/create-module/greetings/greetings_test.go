63cbdcb0235765deb25ad895ca409f52c8345c9e
João <jpedrovieira34@gmail.com>
2025-12-21 22:07:59 -0300

    tutorial: create a module

---
+++ go-by-documentation/create-module/greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Johness"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Johness")
+
+	if !want.MatchString(msg) || err != nil {
+		t.Errorf(`Hello("Johness") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	
+	if msg != "" || err == nil {
+		t.Errorf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
